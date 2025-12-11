6268a7f4b7c1d2ae7d368db40295a19c80d322ff
Maddie09L <madeleinymason20@gmail.com>
2025-12-10 20:39:20 -0500

    Homework20

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -16,6 +17,7 @@ import java.util.UUID;
 public class BaseTest {
 
     public WebDriver driver;
+    public WebDriverWait wait;
     public String url = "https://qa.koel.app/";
 
     @BeforeSuite
@@ -28,8 +30,9 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         url = BaseUrl;
         navigateToPage();
 
